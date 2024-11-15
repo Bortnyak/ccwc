9911ccb5a13f06488bdd3465c6a15c6d17bb5417
Volodymyr Bortniak <volodymyrbortniak@Volodymyrs-MacBook-Pro.local>
2024-11-15 17:26:48 +0100

    finish step one - add c flag

--- wc/wc.go
+++ wc/wc.go
@@ -1,7 +1,36 @@
 package ccwc
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func Run() {
-	fmt.Println("Hello world")
+	cFlag := flag.Bool("c", false, "Enables bytes counting")
+
+	flag.Parse()
+	if *cFlag {
+		// fmt.Println("Bytes counting is turned on")
+
+		args := flag.Args()
+		if len(args) < 1 {
+			fmt.Println("Please provide a file name")
+			return
+		}
+
+		fileName := args[0]
+
+		fileInfo, err := os.Stat(fileName)
+		if err != nil {
+			fmt.Println("Error getting file info:", err)
+			return
+		}
+
+		// Get the size in bytes
+		fileSize := fileInfo.Size()
+
+		fmt.Println(fileSize, fileName)
+	}
+
 }
